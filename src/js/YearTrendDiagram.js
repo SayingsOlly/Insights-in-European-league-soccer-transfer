2adb972481953c2afa5c831b329aaf9f6fc4640b
eric chen <ericchen@lucidchart.com>
2016-12-01 21:03:06 -0700

    some

--- src/js/YearTrendDiagram.js
+++ src/js/YearTrendDiagram.js
@@ -1,3 +1,28 @@
 function YearTrendDiagram() {
-
+    //this.leagues = d3.select('#team-detail-chart-svg').selectAll('div');
+    //
+    //var svgBounds = svg.node().getBoundingClientRect(),
+    //    xAxisWidth = 100,
+    //    yAxisHeight = 70,
+    //    figureWidth = svgBounds.width - xAxisWidth,
+    //    figureHeight = svgBounds.height - yAxisHeight;
+    //
+    //this.yearValues = years.map(function (d) { return d.split('') })
+    //
+    //var xScale = d3.scaleLinear()
+    //    .domain(allWorldCupData.map(yearSelector))
+    //    .range(allWorldCupData.map(function (d, i) {
+    //        return figureWidth - i * figureWidth / dataLength - figureWidth / dataLength / 2;
+    //    }));
+    //
+    //var xAxis = d3.axisBottom();
+    //xAxis.ticks(years.length)
+    //    .scale(xScale);
+    //
+    //svg.select("#xAxis")
+    //    .style("transform", "translate(" + xAxisWidth + "px," + yAxisHeight + "px) scaleY(-1)")
+    //    .call(xAxis)
+    //    .selectAll('text')
+    //    .style("text-anchor", "end")
+    //    .style("transform", "translate(-15px, 10px) rotate(-90deg)");
 }
